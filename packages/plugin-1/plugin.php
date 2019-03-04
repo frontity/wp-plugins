e373304838c7cc76f25dcb26c489e01418151967
David Arenas <darensr@gmail.com>
2019-03-04 10:02:53 +0100

    test version script

---
+++ packages/plugin-1/plugin.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Plugin-1
+Plugin URI: 
+Description: 
+Version: 0.0.1
+Author: 
+ */
+
+define('PLUGIN_VERSION', '0.0.1');
---
+++ version.scripts.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const connect = require("simple-git/promise");
+const replace = require("replace-in-file");
+const packageJson = require(path.join(process.cwd(), './package.json'));
+
+(async () => {
+  const options = {
+    files: "plugin.php",
+    from: [
+      /Version: \d+\.\d+\.\d+/,
+      /define\('PLUGIN_VERSION', '\d+\.\d+\.\d+'\)/,
+    ],
+    to: [
+      `Version: ${packageJson.version}`,
+      `define('PLUGIN_VERSION', '${packageJson.version}')`,
+    ],
+  };
+  try {
+    const changes = await replace(options);
+    console.log("Modified files:", changes.join(", "));
+    const repo = await connect(".");
+    await repo.checkout("master");
+    await repo.add(["plugin.php"]);
+    console.log("Files added.");
+  } catch (error) {
+    console.error("Error occurred:", error);
+  }
+})();
