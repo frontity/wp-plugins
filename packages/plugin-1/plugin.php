d69226ea72e8414693adbce942bf68bdac8b9f44
David Arenas <darensr@gmail.com>
2019-03-04 12:04:57 +0100

    chore(release): publish [skip ci]
     - plugin-1@0.0.2
     - plugin-2@0.0.2

--- packages/plugin-1/plugin.php
+++ packages/plugin-1/plugin.php
@@ -3,8 +3,8 @@
 Plugin Name: Plugin-1
 Plugin URI: 
 Description: 
-Version: 0.0.1
+Version: 0.0.2
 Author: 
  */
 
-define('PLUGIN_VERSION', '0.0.1');
+define('PLUGIN_VERSION', '0.0.2');
--- packages/plugin-2/plugin.php
+++ packages/plugin-2/plugin.php
@@ -3,8 +3,8 @@
 Plugin Name: Plugin-2
 Plugin URI: 
 Description: 
-Version: 0.0.1
+Version: 0.0.2
 Author: 
  */
 
-define('PLUGIN_VERSION', '0.0.1');
+define('PLUGIN_VERSION', '0.0.2');
