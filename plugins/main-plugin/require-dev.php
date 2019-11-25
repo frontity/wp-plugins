2888985fc4fffb33768e89599d3d555f3d20ea46
David <davidarenas@Luiss-iMac.local>
2019-11-25 12:00:49 +0100

    chore(head-tags): get rid of yoast-meta

--- plugins/main-plugin/require-dev.php
+++ plugins/main-plugin/require-dev.php
@@ -1,14 +1,14 @@
 <?php
 
-if (!class_exists("Main_Plugin")) {
-  require_once FRONTITY_MAIN_PATH . "class-main-plugin.php";
+if ( ! class_exists( 'Main_Plugin' ) ) {
+	require_once FRONTITY_MAIN_PATH . 'class-main-plugin.php';
 }
-if (!class_exists("Plugin_One")) {
-  require_once FRONTITY_MAIN_PATH . "../plugin-one/class-plugin-one.php";
+if ( ! class_exists( 'Plugin_One' ) ) {
+	require_once FRONTITY_MAIN_PATH . '../plugin-one/class-plugin-one.php';
 }
-if (!class_exists("Plugin_Two")) {
-  require_once FRONTITY_MAIN_PATH . "../plugin-two/class-plugin-two.php";
+if ( ! class_exists( 'Plugin_Two' ) ) {
+	require_once FRONTITY_MAIN_PATH . '../plugin-two/class-plugin-two.php';
 }
-if (!class_exists("Frontity_Yoast_Meta")) {
-  require_once FRONTITY_MAIN_PATH . "../yoast-meta/class-frontity-yoast-meta.php";
+if ( ! class_exists( 'Frontity_Yoast_Meta' ) ) {
+	require_once FRONTITY_MAIN_PATH . '../frontity-headtags/class-frontity-headtags-plugin.php';
 }
