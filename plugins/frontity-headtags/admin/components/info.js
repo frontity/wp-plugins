acb36914151e36034dd19bff2ca34a3d03031164
David Arenas <darensr@gmail.com>
2019-11-25 13:19:58 +0100

    chore(admin): continue writing the info section

--- plugins/frontity-headtags/admin/components/info.js
+++ plugins/frontity-headtags/admin/components/info.js
@@ -15,19 +15,10 @@ const Info = () => {
       <br />
       <h4>If you are using other JS framework</h4>
       <p>
-        Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-        accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab
-        illo inventore veritatis et quasi architecto beatae vitae dicta sunt
-        explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut
-        odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
-        voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum
-        quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam
-        eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat
-        voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam
-        corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?
-        Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse
-        quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo
-        voluptas nulla pariatur?
+        The plugin adds a new field to entities fetched from the REST API,
+        called <code>head_tags</code>. This field is an array of objects with
+        the properties <code>tag</code>, <code>attributes</code> and{" "}
+        <code>content</code>.
       </p>
       <p>
         At vero eos et accusamus et iusto odio dignissimos ducimus qui
