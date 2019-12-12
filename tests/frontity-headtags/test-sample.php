ab484e6487ceaa4888ea9d03defb9486f2f850ad
David Arenas <darensr@gmail.com>
2019-12-12 19:28:48 +0100

    Add one more test sample

--- tests/frontity-headtags/test-sample.php
+++ tests/frontity-headtags/test-sample.php
@@ -11,10 +11,25 @@
 class HeadTags extends WP_UnitTestCase {
 
 	protected static $post_id;
+	protected static $page_id;
 	protected static $frontity_headtags_plugin;
 
 	public static function wpSetUpBeforeClass( $factory ) {
-		self::$post_id = $factory->post->create();
+
+		// Init a test post.
+		self::$post_id = $factory->post->create(
+			array(
+				'post_title' => 'Post Title',
+			)
+		);
+		
+		// Init a test page.
+		self::$page_id = $factory->post->create(
+			array(
+				'post_type'  => 'page',
+				'post_title' => 'Page Title',
+			)
+		);
 
 		// Activate plugin.
 		self::$frontity_headtags_plugin = new Frontity_Headtags_Plugin();
@@ -37,6 +52,18 @@ class HeadTags extends WP_UnitTestCase {
 		$response = rest_get_server()->dispatch( $request );
 		$data     = $response->get_data();
 		$this->assertEquals( 'title', $data['head_tags'][0]['tag'] );
-		$this->assertEquals( 'Post title 18 – Test Blog', $data['head_tags'][0]['content'] );
+		$this->assertEquals( 'Post Title – Test Blog', $data['head_tags'][0]['content'] );
+	}
+
+	/**
+	 * Another single example test.
+	 */
+	public function test_context_param_with_page() {
+		$request = new WP_REST_Request( 'GET', sprintf( '/wp/v2/pages/%d', self::$page_id ) );
+		$request->set_query_params( array( 'head_tags' => 'true' ) );
+		$response = rest_get_server()->dispatch( $request );
+		$data     = $response->get_data();
+		$this->assertEquals( 'title', $data['head_tags'][0]['tag'] );
+		$this->assertEquals( 'Page Title – Test Blog', $data['head_tags'][0]['content'] );
 	}
 }
