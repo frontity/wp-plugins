83fe6ea70a168ed9362fa2cb7f8c4f1d4a73963c
David Arenas <darensr@gmail.com>
2019-12-18 12:01:11 +0100

    Fix phpcs errors in frontity-headtags

--- plugins/frontity-headtags/admin/index.php
+++ plugins/frontity-headtags/admin/index.php
@@ -5,20 +5,28 @@
  * @package Frontity_Headtags
  */
 
-$plugin_dir_url = FRONTITY_HEADTAGS_URL;
-$loc            = get_locale();
-$settings       = get_option( 'frontity_headtags_settings' );
-?>
-
+/**
+ * Generate the HTML code of the admin page.
+ */
+function frontity_headtags_admin() {
+	$plugin_dir_url = FRONTITY_HEADTAGS_URL;
+	$loc            = get_locale();
+	$settings       = get_option( 'frontity_headtags_settings' );
+	?>
 <div id='root'></div>
 <script>
 window.frontity = {
-locale: <?php echo wp_json_encode( $loc ? $loc : '' ); ?>,
-plugins: {
-headtags: {
-	url: <?php echo wp_json_encode( $plugin_dir_url ? $plugin_dir_url : '' ); ?>,
-	settings: <?php echo wp_json_encode( $settings ? $settings : new stdClass() ); ?>,
-}
-}
+	locale: <?php echo wp_json_encode( $loc ? $loc : '' ); ?>,
+	plugins: {
+		headtags: {
+			url: <?php echo wp_json_encode( $plugin_dir_url ? $plugin_dir_url : '' ); ?>,
+			settings: <?php echo wp_json_encode( $settings ? $settings : new stdClass() ); ?>,
+		}
+	}
 };
 </script>
+	<?php
+}
+
+// Call to the function.
+frontity_headtags_admin();
