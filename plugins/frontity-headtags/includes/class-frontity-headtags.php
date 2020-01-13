7b77561798a520e0d691dc2d6cb7a327be1d989c
David Arenas <darensr@gmail.com>
2020-01-13 15:18:45 +0100

    Change query params to skip caching

--- plugins/frontity-headtags/includes/class-frontity-headtags.php
+++ plugins/frontity-headtags/includes/class-frontity-headtags.php
@@ -107,11 +107,15 @@ class Frontity_Headtags {
 	 * @return array|mixed
 	 */
 	public function get_headtags( $key, $query ) {
-		// phpcs:ignore WordPress.Security.NonceVerification.Recommended
-		$ignore_cache = isset( $_GET['ignore_cache'] ) && 'true' === $_GET['ignore_cache'];
+		// phpcs:disable WordPress.Security.NonceVerification.Recommended
+		// phpcs:disable WordPress.Security.ValidatedSanitizedInput.InputNotSanitized
+		$skip_cache = isset( $_GET['head_tags_skip_cache'] )
+			? wp_validate_boolean( $_GET['head_tags_skip_cache'] )
+			: isset( $_GET['skip_cache'] );
+		// phpcs:enable
 
 		// Get head tags from transients.
-		$headtags = $ignore_cache ? null : $this->get_cached_headtags( $key );
+		$headtags = $skip_cache ? null : $this->get_cached_headtags( $key );
 
 		// If head tags are not cached, compute and cache them.
 		if ( empty( $headtags ) ) {
